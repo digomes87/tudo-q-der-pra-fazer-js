421dd450396bcad30dd2b1799dab644633c69a0a
Diego Go <Diego.Gomes87@gmail.com>
2021-03-19 16:49:30 -0300

    Videooooo, até hoje me sinto muito feliz em fazer a webcam ligar atravez de codigo, é muito legal

--- py/pyAquiPqEuQuis/tratamento_de_imagem.py
+++ py/pyAquiPqEuQuis/tratamento_de_imagem.py
@@ -6,6 +6,23 @@ print(ima1)
 
 cv2.imshow('image', ima1)
 cv2.waitKey(0)
-cv2.destroyAllWindons()
+cv2.destroyAllWindows()
 
+#fazendo copia da foto com efeito
+cv2.imwrite('Euu.jpeg', ima1)
+
+
+
+video = cv2.VideoCapture(0)
+
+while(True):
+    ret , frame = video.read()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindons()
 
