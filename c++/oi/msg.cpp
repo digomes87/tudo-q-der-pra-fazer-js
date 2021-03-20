113301d3b07e9fe9c9dd469ac0107867b3bd7101
Diego Go <Diego.Gomes87@gmail.com>
2021-03-20 14:24:29 -0300

    Que curioso, a subtracao funcionou porem inverteu o sinal

--- c++/oi/msg.cpp
+++ c++/oi/msg.cpp
@@ -11,7 +11,12 @@ int main() {
 	cin>>z;
 
 	int diminui = z-x;
-
+	int soma = x+z;
+	int multiplicacao = x*z;
+	int divisao = x*z;
+	cout << "A soma: " << soma <<endl;
+	cout << "A divisao: " << divisao <<endl;
+	cout << "A multiplcacao: " << multiplicacao <<endl;
 	cout << "Subtraindo os dados, resultado: "<< diminui << endl;
 
 	return 0;
