c657f5336a2572e987c80ca904722e7ed18f3460
Diego Go <Diego.Gomes87@gmail.com>
2021-03-20 21:43:53 -0300

    Compilar jogos com a Library SFML é complicadinho mas consegui

--- c++/jogos/teste.cpp
+++ c++/jogos/teste.cpp
@@ -1,24 +1,20 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+int main() {
+       sf::RenderWindow window(sf::VideoMode(640, 480), "Funcionou essa Bosta");
+       sf::CircleShape shape;
+       shape.setRadius(40.f);
+       shape.setPosition(100.f, 100.f);
+       shape.setFillColor(sf::Color::Cyan);
+       while (window.isOpen()){
+       		sf::Event event;
+       while (window.pollEvent(event)){
+           if (event.type == sf::Event::Closed)
+               window.close();
+       }
+      
+       window.clear();
+       window.draw(shape);
+       window.display();
+       }
 }
