af123f3d2e162f3b306e3d47536640dbc9a98ae1
Diego Go <Diego.Gomes87@gmail.com>
2021-03-21 17:03:21 -0300

    Adicionado api para consumo

--- js/covidbrasil/src/App.js
+++ js/covidbrasil/src/App.js
@@ -5,10 +5,24 @@ class App extends React.Component{
     constructor(){
         super();
         this.state = {
-            name: 'Diego',
+            contries:[],
+            stats: []
         }
     }
-   render(){
+    async componentDidMount() {
+        const resp = await fetch('https://api.covid19api.com/countries')
+        const countries = await resp.json()
+        this.setState({countries})
+        this.state.countries.forEach(async country =>{
+            const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
+            const data = await resp.json()
+
+            this.setState(prevState => (
+                {stats:prevState.stats.concat(data[data.length - 1])}
+            ))
+        })
+    }
+    render(){
     return(
         this.state.name
     )
