21238cbd69840ecbb88a3ef108e750b8cdf63d1f
Diego Go <Diego.Gomes87@gmail.com>
2021-03-21 20:50:53 -0300

    adicionando css, faltar terminar o css do country

--- js/covidbrasil/src/components/Country/Country.js
+++ js/covidbrasil/src/components/Country/Country.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import '../../Country.css'
 const Country = (props) =>{
 
     return(
-        <div>
+        <div className='country'>
                 <img src={`https://www.countryflags.io/${props.stats.CountryCode}/flat/64.png`} alt={props.stats.Country}></img>
                 {props.stats.Country}
-            <div>
+            <div className='descricao'>
                 <p>{`Ativos : ${props.stats.Active}`}</p>
                 <p>{`Confirmados : ${props.stats.Confirmed}`}</p>
                 <p>{`Mortos : ${props.stats.Deaths}`}</p>
