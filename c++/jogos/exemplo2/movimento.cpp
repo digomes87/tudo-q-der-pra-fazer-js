22229105ebd7d6f91aff61557ba1010407a3d6cf
Diego Go <Diego.Gomes87@gmail.com>
2021-03-23 14:07:29 -0300

    OO in c++ is beatiful

---
+++ c++/jogos/exemplo2/movimento.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using std::string;
+
+class Employee{
+  public:
+    std::string Nome;
+    std::string Company;
+    int age;
+
+    void IntroduceYourSelf(){
+        std::cout<<"My Name - "<<Nome<<std::endl;
+        std::cout<<"Companhia - "<<Company<<std::endl;
+        std::cout<<"idade - "<<age<<std::endl;
+    }
+
+};
+
+
+int main(){
+    Employee employee1;
+    employee1.Nome = "Diego";
+    employee1.Company = "PyCoorp";
+    employee1.age = 33;
+
+    employee1.IntroduceYourSelf();
+
+    Employee employee2;
+    employee2.Nome = "Tiago";
+    employee2.Company = "Google";
+    employee2.age = 45;
+
+    employee2.IntroduceYourSelf();
+}
+
