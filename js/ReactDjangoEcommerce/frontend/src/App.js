b4ef50395502c7024ba0bf52d456e7dccfa98de6
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2021-05-08 12:39:41 -0300

    Add bootstrap

--- js/ReactDjangoEcommerce/frontend/src/App.js
+++ js/ReactDjangoEcommerce/frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div>
-      Aqui tudo comeca 
+      <Header/>
+        <main className="py-3">
+          <Container>
+            <h1>Welcome</h1>
+          </Container>
+        </main>
+      <Footer />
     </div>
   );
 }
